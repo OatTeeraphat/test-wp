7760f249440b6311c4f7d58e0702b112e0764caa
oatteeraphat <oateerapat@gmail.com>
2016-09-10 15:11:18 +0700

    1st commit

---
+++ oat.js
@@ -0,0 +1,77 @@
+/*function x(){
+	// calculate sth...
+  setTimeout(function(){
+
+    return 123;
+  }, 500)
+  //return 123;
+ }
+ 
+ result = x()
+ alert(result)
+ 
+ */
+ 
+ var app = {
+ 	get: function(path, cb){
+  	if(path==='/oat') {
+    	var res = {
+      	send: function(text){ 
+        	console.log(text) 
+        }
+      }
+    	cb(null, res);
+		}
+  }
+ }
+ 
+ // query Mongo
+ var doSmallWork = function(input, cb){
+ 	setTimeout(function(){ 
+  	cb(++input)
+   }, 500);
+ }
+ 
+ // query SQL
+ var doHardWork = function(input, cb){
+ 	setTimeout(function(){ 
+  	input += 2;
+  	cb(input)
+   }, 1000);
+ }
+
+ // Express
+ app.get('/oat', function(req, res){
+ 	var input = 0;
+
+  for(var i=0; i<3; i++){
+  	doSmallWork(input, function(result){
+    	console.log(result)
+    })
+  }
+
+  /*doHardWork(input, function(result){
+  	res.send(result);
+    doSmallWork(result, res.send)
+  })*/
+ 
+  /*doSmallWork(input, function(result){
+  	res.send(result)
+  	doSmallWork(result, function(result){
+    	res.send(result)
+    	doSmallWork(result, res.send)
+    })
+  })*/
+})
+
+
+PublisherService = {
+	getPubByPub_id: function(Pub_id, cb){
+  	Publishers.findById(Pub_id).populate('columns').exec(cb);
+  }
+}
+
+PublisherService.getPubByPub_id(Pub_id, function(err, pub){
+	if(!err) res.json(pub)
+})
+ 
---
+++ page-news.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Page News
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div class="container">
+			<div class="h1"> page news</div>
+		</div>
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
